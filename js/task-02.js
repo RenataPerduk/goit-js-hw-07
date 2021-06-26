cec41c3c807e752665b70ee1764fc51f145f316f
RenataPerduk <perdukrenata@gmail.com>
2021-06-26 20:17:40 +0300

    Результат

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+  parent.append(...items)
+});
+
+console.log(foodIngredients);
